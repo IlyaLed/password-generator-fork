71cad7c3b83682148b4f70e96e832234731c3252
Ilya Lednev <vl.lednev16082005@gmail.com>
2025-04-24 13:07:29 +1000

    Добавлен генератор паролей

---
+++ password.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+string generatePassword(int length, bool useDigits, bool useSymbols) {
+    const string letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const string digits = "0123456789";
+    const string symbols = "!@#$%^&*()_+-=[]{}|;:,.<>?";
+
+    string chars = letters;
+    if (useDigits) chars += digits;
+    if (useSymbols) chars += symbols;
+
+    srand(time(0));
+    string password;
+    for (int i = 0; i < length; ++i) {
+        password += chars[rand() % chars.size()];
+    }
+    return password;
+}
+
+int main() {
+    int length;
+    bool digits, symbols;
+
+    cout << "Длина пароля: ";
+    cin >> length;
+    cout << "Цифры (1/0): ";
+    cin >> digits;
+    cout << "Спецсимволы (1/0): ";
+    cin >> symbols;
+
+    string password = generatePassword(length, digits, symbols);
+    cout << "Ваш пароль: " << password << endl;
+
+    return 0;
+}
